4981b72c6203de8296c60001eb224b869f62142e
Abdulhameed26 <61632353+Abdulhameed26@users.noreply.github.com>
2020-03-29 03:37:12 +0200

    review login page

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PahramcyOnline
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/jquery-3.3.1.min.js", "~/Scripts/jquery-ui.js",
+                      "~/Scripts/popper.min.js", "~/Scripts/owl.carousel.min.js",
+                      "~/Scripts/owl.carousel.min.js", "~/Scripts/jquery.magnific-popup.min.js",
+                      "~/Scripts/aos.js", "~/Scripts/js/main.js", "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/Login/bootstrap").Include(
+                      "~/Scripts/Login/cartoon.js"));
+
+
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/magnific-popup.css", "~/Content/jquery-ui.css",
+                      "~/Content/owl.carousel.min.css", "~/Content/owl.theme.default.min.css",
+                      "~/Content/aos.css", "~/Content/style.css", "~/fonts/icomoon/style.css"
+                     ));
+
+            bundles.Add(new StyleBundle("~/Content/Login/css").Include(
+                      "~/Content/Login/index.css"
+                      ));
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PahramcyOnline.MvcApplication" Language="C#" %>
