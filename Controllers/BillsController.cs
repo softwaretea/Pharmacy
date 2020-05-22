03e34814503f7f6859b0a209c2ef24c2976b5a6f
rahma <rahma.usama17@gmail.com>
2020-05-22 03:25:12 +0200

    Bill Controller and view

---
+++ Controllers/BillsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PahramcyOnline.Models;
+
+namespace PahramcyOnline.Controllers
+{
+    public class BillsController : Controller
+    {
+        private pharmacyEntities db = new pharmacyEntities();
+
+        // GET: Bills
+        public ActionResult Index()
+        {
+            var bills = db.Bills.Include(b => b.User);
+            return View(bills.ToList());
+        }
+
+        // GET: Bills/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bill);
+        }
+
+        // GET: Bills/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_user = new SelectList(db.Users, "Id_user", "fisrt_name");
+            return View();
+        }
+
+        // POST: Bills/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_user,added_at,sum")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bills.Add(bill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_user = new SelectList(db.Users, "Id_user", "fisrt_name", bill.id_user);
+            return View(bill);
+        }
+
+        // GET: Bills/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_user = new SelectList(db.Users, "Id_user", "fisrt_name", bill.id_user);
+            return View(bill);
+        }
+
+        // POST: Bills/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_user,added_at,sum")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_user = new SelectList(db.Users, "Id_user", "fisrt_name", bill.id_user);
+            return View(bill);
+        }
+
+        // GET: Bills/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bill);
+        }
+
+        // POST: Bills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bill bill = db.Bills.Find(id);
+            db.Bills.Remove(bill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
